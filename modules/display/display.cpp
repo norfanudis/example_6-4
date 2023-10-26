57c7624fefe874c10cb2b947c28ac4c22a96fa09
norfanudis <norfanudis@fi.uba.ar>
2023-10-26 22:31:38 +0000

    Agregado control de impresion de fila y columna de lo que esta imprimiendo el display. Agregado el arbol de dependencias de display.cpp

--- modules/display/display.cpp
+++ modules/display/display.cpp
@@ -108,6 +108,10 @@ DigitalOut displayD7( D7 );
 DigitalOut displayRs( D8 );
 DigitalOut displayEn( D9 );
 
+/*9.a
+- `PB_9` (I2C1_SDA) y `PB_8` (I2C1_SCL): Estos pines se utilizan para la comunicación I2C con un 
+módulo PCF8574 IO Expander.*/
+
 I2C i2cPcf8574( I2C1_SDA, I2C1_SCL ); 
 
 DigitalOut spiSt7920ChipSelect(SPI1_CS);
@@ -130,6 +134,64 @@ static void displayDataBusWrite( uint8_t dataByte );
 static void displayCodeWrite( bool type, uint8_t dataBus );
 
 //=====[Implementations of public functions]===================================
+/*
+9.b
+El tipo de interfaz y su variante se determinan a través de la variable display.connection,
+ que se utiliza para especificar el tipo de conexión utilizada. Hay tres opciones posibles en 
+ el código según el tipo de dato definido en display.h, displayConnection_t.
+
+DISPLAY_CONNECTION_I2C_PCF8574_IO_EXPANDER: En esta opción, se utiliza un módulo PCF8574 IO 
+Expander para controlar el display a través de una interfaz I2C. Los pines I2C I2C1_SDA y I2C1_SCL 
+se utilizan para la comunicación I2C con el módulo PCF8574.
+*/
+
+/*
+9.c
+------------------Arbol de displayInit en display.cpp--------------------
+- displayInit(displayConnection_t connection) en display.cpp
+  - Llama a:
+    - delay(int milliseconds) en arm_book_lib.h 
+    - displayCodeWrite(bool type, uint8_t dataBus)
+      - Llama a:
+        - displayPinWrite(uint8_t pinName, int value)
+        - displayDataBusWrite(uint8_t dataBus)
+  - Dependencias:
+    - mbed.h
+    - arm_book_lib.h 
+
+- delay(int milliseconds) en arm_book_lib.h
+  - Llama a:
+    - thread_sleep_for(ms) en mbed.h
+  - Dependencias:
+    - mbed.h 
+
+-thread_sleep_for(ms) en mbed.h
+  - Llama a:
+    - do_timed_sleep_absolute (default que utiliza el clock del sistema operativo)
+    - sleep_until ( si se define el flag MBED_CONF_RTOS_PRESENT se utiliza el clock del thread del kernel)
+  - Dependencias:
+    - platform/internal/mbed_os_timer.h 
+    - "rtos/Kernel.h" y "rtos/ThisThread.h" (MBED_CONF_RTOS_PRESENT si está definida)
+
+- displayCodeWrite(bool type, uint8_t dataBus) en display.cpp
+  - Llama a:
+    - displayPinWrite(uint8_t pinName, int value)
+    - displayDataBusWrite(uint8_t dataBus)
+  - I2C.h
+
+- displayPinWrite(uint8_t pinName, int value) en display.cpp
+  - Llama a:
+    - write en I2C
+  - Dependencia:
+    - I2C
+ 
+- displayDataBusWrite(uint8_t dataBus) en display.cpp
+  - Llama a:
+    - displayPinWrite(uint8_t pinName, int value) en display.cpp
+    - delay(int milliseconds) en arm_book_lib.h 
+  - Dependencias:
+    - arm_book_lib.h
+*/
 
 void displayInit( displayType_t type, displayConnection_t connection )
 {
